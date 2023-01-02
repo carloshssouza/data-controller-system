22098c2e72e56dcd911d9b2200766ff0282039d5
Carlos Souza <carlossouza_94@hotmail.com>
2023-01-01 23:54:50 -0300

    feat: add proxy server

---
+++ proxy-server/src/index.js
@@ -0,0 +1,25 @@
+const httpProxy = require('http-proxy');
+const proxy = httpProxy.createProxyServer();
+const option = {
+    target:'http://localhost:9000',
+    selfHandleResponse : true
+  };
+
+proxy.on('proxyRes', function (proxyRes, req, res) {
+    var body = [];
+    proxyRes.on('data', function (chunk) {
+        body.push(chunk);
+    });
+    proxyRes.on('end', function () {
+        body = Buffer.concat(body).toString();
+        console.log("res from proxied server:", body);
+        res.end(JSON.stringify({body: "proxy", message: "Teste"}));
+    });
+});
+
+const server = require('http').createServer((req, res) => {
+    proxy.web(req, res, option);
+  });
+
+server.listen(3000);
+console.log("Proxy server listening on 3000")
