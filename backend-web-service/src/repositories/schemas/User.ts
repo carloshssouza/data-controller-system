0e8ec42d0b953a299822880df6a5ed3aa5f84d8b
Carlos Souza <carlossouza_94@hotmail.com>
2023-01-03 23:56:18 -0300

    feat: add repositories and schemas

---
+++ backend-web-service/src/repositories/schemas/User.ts
@@ -0,0 +1,14 @@
+
+import { Schema, model } from 'mongoose'
+import { IUser } from '../interfaces/interfaces.schemas'
+
+const UserSchema = new Schema<IUser>({
+  name: { type: String, required: true },
+  email: { type: String, required: true, unique: true },
+  password: { type: String, require: true }
+}, {
+  timestamps: true,
+  versionKey: false
+})
+
+export default model('User', UserSchema)
