3e2a7f0e05abea04d74dd37416882bfc3f88ade5
Carlos Souza <carlossouza_94@hotmail.com>
2023-01-08 01:44:42 -0300

    feat: add api and logError repositories

---
+++ backend-web-service/src/repositories/api.repository.ts
@@ -0,0 +1,25 @@
+import { ApiCreateData, ApiUpdateData } from '../interfaces/api'
+import { TypeId } from '../types/mongoose'
+import Api from './schemas/Api'
+
+export default class ApiRepository {
+  public createApi (data: ApiCreateData) {
+    return Api.create(data)
+  }
+
+  public getApi (_id: TypeId) {
+    return Api.findOne({ _id })
+  }
+
+  public getAllApis () {
+    return Api.find({})
+  }
+
+  public updateApi (_id: TypeId, data: ApiUpdateData) {
+    return Api.findOneAndUpdate({ _id }, data)
+  }
+
+  public deleteApi (_id: TypeId) {
+    return Api.findOneAndDelete({ _id })
+  }
+}
