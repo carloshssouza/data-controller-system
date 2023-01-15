d38cefa06223e7e2136296fcc7798c6c5d31f9e9
Carlos Souza <carlossouza_94@hotmail.com>
2023-01-15 01:27:03 -0300

    feat: start grpc server

--- backend-web-service/src/index.ts
+++ backend-web-service/src/index.ts
@@ -1,7 +1,10 @@
 import app from './app'
+import grpc from './grpc'
 import dotenv from 'dotenv'
+
 dotenv.config()
 
 const PORT = process.env.PORT || process.env.APP_PORT
 
+grpc.start()
 app.listen(PORT, () => console.log(`App listening on port ${PORT}`))
