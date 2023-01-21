49c686457bda21b612ec6af2790052b362793ebc
Carlos Souza <carlossouza_94@hotmail.com>
2023-01-21 03:07:35 -0300

    feat: add types

---
+++ proxy-server/src/types/grpc.ts
@@ -0,0 +1,15 @@
+import { PackageDefinition } from '@grpc/grpc-js/build/src/make-client'
+import {
+  GrpcObject,
+  ServiceClientConstructor,
+  ProtobufTypeDefinition,
+  Client
+} from '@grpc/grpc-js'
+
+export {
+  PackageDefinition,
+  GrpcObject,
+  ServiceClientConstructor,
+  ProtobufTypeDefinition,
+  Client
+}
