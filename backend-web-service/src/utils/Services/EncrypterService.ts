ba3d731d977ebb8f0f87d0d06731c93c52de63df
Carlos Souza <carlossouza_94@hotmail.com>
2023-01-27 23:26:10 -0300

    docs: doc method encrypter

--- backend-web-service/src/utils/Services/EncrypterService.ts
+++ backend-web-service/src/utils/Services/EncrypterService.ts
@@ -1,10 +1,21 @@
 import bcrypt from 'bcrypt'
 
 class Encrypter {
+  /**
+   *
+   * @param data Password data
+   * @param hash Password hash
+   * @returns Returns boolean
+   */
   async compare (data: any, hash: string): Promise<boolean> {
     return bcrypt.compare(data, hash)
   }
 
+  /**
+   *
+   * @param data Password to be encrypt
+   * @returns Returns the password encrypted
+   */
   async hash (data: any): Promise<string> {
     return bcrypt.hash(data, 8)
   }
