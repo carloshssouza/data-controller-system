ff83065eba05497dc7505031fcfbab16a7da5458
Carlos Souza <carlossouza_94@hotmail.com>
2023-01-28 23:49:08 -0300

    fix: fix the repository error log

--- backend-web-service/src/repositories/schemas/ErrorLog.ts
+++ backend-web-service/src/repositories/schemas/ErrorLog.ts
@@ -11,8 +11,9 @@ const ErrorLogSchema = new Schema<IErrorLog>({
   title: { type: String, required: true },
   description: { type: String, required: true },
   routeId: { type: Schema.Types.ObjectId, required: true, ref: 'Api' },
+  endpointPath: { type: String, required: true, ref: 'Api' },
   routeName: { type: String, required: true, ref: 'Api' },
-  leakData: { type: [LeakedDataSchema], required: true },
+  leakedData: { type: [LeakedDataSchema], required: true },
   level: { type: String, required: true, enum: ['low', 'medium', 'high'] }
 }, {
   timestamps: true,
