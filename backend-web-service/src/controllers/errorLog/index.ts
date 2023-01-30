bd616ec7a1c99322de1ddf9df297255e0148c360
Carlos Souza <carlossouza_94@hotmail.com>
2023-01-29 23:25:57 -0300

    feat: add new error log controllers

--- backend-web-service/src/controllers/errorLog/index.ts
+++ backend-web-service/src/controllers/errorLog/index.ts
@@ -2,10 +2,14 @@ import ErrorLogGetController from './errorLog.get.controller'
 import ErrorLogUpdateController from './errorLog.update.controller'
 import ErrorLogGetAllController from './errorLog.getAll.controller'
 import ErrorLogDeleteController from './errorLog.delete.controller'
+import ErrorLogLeakedDataController from './errorLog.leakedData.controller'
+import ErrorLogLeakedDataByApiController from './errorLog.leakedDataByApi.controller'
 
 export {
   ErrorLogGetController,
   ErrorLogUpdateController,
   ErrorLogGetAllController,
-  ErrorLogDeleteController
+  ErrorLogDeleteController,
+  ErrorLogLeakedDataController,
+  ErrorLogLeakedDataByApiController
 }
