24f8e17c2d3ee35c57da381e6cfe1f3a2aef4f3c
Carlos Souza <carlossouza_94@hotmail.com>
2023-01-30 23:42:14 -0300

    refactor: fix the routes

--- backend-web-service/src/routes/errorLog.routes.ts
+++ backend-web-service/src/routes/errorLog.routes.ts
@@ -4,7 +4,7 @@ import {
   ErrorLogUpdateController,
   ErrorLogDeleteController,
   ErrorLogLeakedDataController,
-  ErrorLogLeakedDataByApiController
+  ErrorLogDynamicFilterController
 } from '../controllers/errorLog'
 import { Router } from '../types/express'
 // import Authenticate from '../middleware/Authenticate'
@@ -16,18 +16,6 @@ routes.get('/error-log', ErrorLogGetAllController.getAllErrorLogs)
 routes.put('/error-log/:_id', ErrorLogUpdateController.updateErrorLog)
 routes.delete('/error-log/:_id', ErrorLogDeleteController.deleteErrorLog)
 routes.get('/error-log/filter/leaked-data', ErrorLogLeakedDataController.getErrorLogLeakedData)
-
-routes.get('/error-log/filter/leaked-data-by-api', ErrorLogLeakedDataByApiController.getErrorLogLeakedDataByApi)
-
-// Quantidade de dados vazados por todas api
-
-// // Quantidade de dados vazados por api, req param com id da api e req.query para pessoal ou sensivel
-// routes.get('/error-log/amount-leaked-data-by-api/:_id', ErrorLogAmountLeakedDataByApiController.getErrorLogAmountLeakedDataByApi)
-
-// // Quantidade de vazamento pelo level, considerar de todos, dos sensivels e pessoais, req.query
-// routes.get('/error-log/amount-leaked-data-level', ErrorLogAmountLeakedDataLevel.getErrorLogAmountLeakedDataLevel)
-
-// // Busca inteligente que considera todos os filtros possíveis
-// routes.post('error-log/filter-search', ErrorLogFilterSearchController.getErrorLogDataByFilter)
+routes.post('/error-log/dynamic-filter', ErrorLogDynamicFilterController.getErrorLogDynamicFilter)
 
 export default routes
