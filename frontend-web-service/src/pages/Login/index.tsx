928969c2d6afdc6e2a160bd454bacc90390d56a0
Carlos Souza <carlossouza_94@hotmail.com>
2023-02-04 23:51:32 -0300

    feat: add login, api, error log pages

--- frontend-web-service/src/pages/Login/index.tsx
+++ frontend-web-service/src/pages/Login/index.tsx
@@ -1,7 +1,46 @@
-import React from 'react'
+import React, { useCallback, useEffect, useState } from "react";
+import Input from "../../components/Input";
 
-export default function Login() {
+const Login = () => {
+  const [loginData, setLoginData] = useState({
+    email: "",
+    password: "",
+  })
+
+  const onChangeLogin = useCallback((event: React.ChangeEvent<HTMLInputElement>) => {
+    const { name, value } = event.target
+    setLoginData((prevLoginData) => ({ ...prevLoginData, [name]: value }));
+  }, [loginData])
+
+  const handleLoginData = (event: React.MouseEvent<HTMLButtonElement>) => {
+    event.preventDefault()
+    console.log(loginData)
+  }
+
   return (
-    <div>Login</div>
-  )
-}
+    <div style={{ backgroundColor: 'red' }}>
+      <div style={{ backgroundColor: "yellow" }}>
+        <Input
+          width="200"
+          placeholder="Email"
+          type="email"
+          name="email"
+          onChange={(event)=> onChangeLogin(event)}
+          required
+        />
+        <Input
+          width="200"
+          placeholder="Password"
+          type="password"
+          name="password"
+          onChange={(event)=> onChangeLogin(event)}
+          required
+        />
+      </div>
+
+      <button onClick={(event) => handleLoginData(event)}>Register</button>
+    </div>
+  );
+};
+
+export default Login;
---
+++ frontend-web-service/src/pages/index.tsx
@@ -0,0 +1,11 @@
+import Login from './Login'
+import Dashboard from './Dashboard'
+import Api from './Api'
+import Error from './Error'
+
+export {
+  Login,
+  Dashboard,
+  Api,
+  Error
+}
