5f887d4919708e76451eabc69236e47f130949f9
Carlos Souza <carlossouza_94@hotmail.com>
2023-02-04 23:51:58 -0300

    feat: add global styles

---
+++ frontend-web-service/src/themes/Themes.tsx
@@ -0,0 +1,10 @@
+export const lightTheme = {
+  body: '#FFF',
+  text: '#363537',
+  background: '#363537',
+}
+export const darkTheme = {
+  body: '#27293D',
+  text: '#FAFAFA',
+  background: '#999',
+}
