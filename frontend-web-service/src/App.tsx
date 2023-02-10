84f49c3204a8826942e85618b53d4dd86c06151b
Carlos Souza <carlossouza_94@hotmail.com>
2023-02-09 23:56:44 -0300

    feat: import bootstrap and add global styles

--- frontend-web-service/src/App.tsx
+++ frontend-web-service/src/App.tsx
@@ -1,32 +1,12 @@
-import Login from './pages/Login'
-import { ThemeProvider, createGlobalStyle } from 'styled-components';
-import { lightTheme, darkTheme } from './themes/Themes'
-
 import Routes from './routes'
-import { useState } from 'react';
-
-const ThemeType = {
-  body: lightTheme,
-  text: darkTheme
-
-}
-export const GlobalStyles = createGlobalStyle<{ theme: typeof ThemeType }>`
-  @import url('https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,300;0,700;1,400;1,700&display=swap');
-  body {
-    background: ${({ theme }) => theme.body};
-    color: ${({ theme }) => theme.text};
-    font-family: Roboto, sans-serif;
-    transition: all 0.50s linear;
-  }`
-
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { GlobalStyles } from './GlobalStyles';
 
 function App() {
   return (
     <>
-        <ThemeProvider theme={darkTheme}>
-          <GlobalStyles />
-        </ThemeProvider>
-        <Routes />
+      <GlobalStyles />
+      <Routes />      
     </>
   )
 }
