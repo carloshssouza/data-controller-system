4a7a1895faaf6e004119fdd47cb9fb1fd3576f13
Carlos Souza <carlossouza_94@hotmail.com>
2023-02-12 23:48:18 -0300

    refactor: delete files

--- frontend-web-service/src/components/Input/styles.tsx
+++
@@ -1,7 +0,0 @@
-import styled from 'styled-components'
-
-export const InputStyle = styled.input`
-  width: ${props => `${props.width}px`};
-  height: ${props => `${props.height}px`};
-  outline: none;
-`
