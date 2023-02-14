0270b980fbf491c2422063ae199df015c7b5f6cf
Carlos Souza <carlossouza_94@hotmail.com>
2023-02-13 23:58:13 -0300

    refactor: update the component api

--- frontend-web-service/src/pages/Api/index.tsx
+++ frontend-web-service/src/pages/Api/index.tsx
@@ -5,6 +5,8 @@ import { ToastContainer, toast } from 'react-toastify';
 import { Button, Checkbox, Form, Input, Modal, Select, Table, Popconfirm } from 'antd';
 
 import { ApiAddContainer, ApiListContainer } from './styles';
+import ModalUpdateApiComponent from '../../components/ModalUpdateApiComponent';
+import FormAddApiComponent from '../../components/FormAddApiComponent';
 
 interface IApiData {
   routeName: string;
@@ -18,8 +20,8 @@ const requestType = ['GET', 'POST', 'PUT', 'PATCH', 'DELETE']
 export default function Api() {
   const [listApisData, setListApisData] = useState([])
   const [selectRequestType, setSelectRequestType] = useState(requestType[0])
-  const [updateModalVisible, setUpdateModalVisible] = useState(false);
   const [selectedRecord, setSelectedRecord] = useState<any>();
+  const [updateModalVisible, setUpdateModalVisible] = useState(false);
   const [apiUpdateData, setApiUpdateData] = useState({
     routeName: '',
     endpointPath: '',
@@ -140,12 +142,12 @@ export default function Api() {
       key: "actions",
       render: (text: string, record: any) => (
         <>
-          <Button onClick={() => handleUpdate(record)}>Update</Button>
+          <Button type="primary" onClick={() => handleUpdate(record)}>Update</Button>
           <Popconfirm
             title="Are you sure you want to delete this item?"
             onConfirm={() => deleteApi(record._id)}
           >
-            <Button>Delete</Button>
+            <Button style={{background: "#E5484D"}}>Delete</Button>
           </Popconfirm>
         </>
       ),
@@ -178,109 +180,23 @@ export default function Api() {
     <>
       <ApiAddContainer>
         <h1>Add new APIs</h1>
-        <div>
-          <Form
-            name="basic"
-            initialValues={{ remember: true }}
-            onFinish={createApi}
-            autoComplete="off"
-          >
-            <Form.Item
-              name="routeName"
-              rules={[{ required: true, message: 'Route name is required' }]}
-            >
-              <Input placeholder="Route name" />
-            </Form.Item>
-            <Form.Item
-              name="endpointPath"
-              rules={[{ required: true, message: 'Endpoint path is required' }]}
-            >
-              <Input placeholder="Endpoint path" />
-            </Form.Item>
-            <Form.Item
-              name="requestType"
-              rules={[{ required: true, message: 'Request type is required' }]}
-            >
-              <Select
-                defaultValue={apiUpdateData.requestType}
-                style={{ width: 150 }}
-                onChange={handleChangeRequestType}
-                options={requestType.map((type: string) => {
-                  return {
-                    label: type,
-                    value: type
-                  }
-                })}
-              />
-            </Form.Item>
-            <Form.Item
-              name="dataReturnAllowed"
-              valuePropName="checked"
-              style={{ marginRight: "2rem" }}
-            >
-              <Checkbox style={{color: "white"}}>Return personal and sensible data</Checkbox>
-            </Form.Item>
-            <Button type="primary"  htmlType="submit">Add</Button>
-          </Form>
-        </div>
+        <FormAddApiComponent
+          createApi={createApi}
+          apiUpdateData={apiUpdateData}
+          handleChangeRequestType={handleChangeRequestType}
+          requestType={requestType}
+        />
       </ApiAddContainer>
       <ApiListContainer>
         <h1>List of APIs</h1>
         <Table columns={columns} dataSource={listApisData} />
-        <Modal
-          title="Update Route"
-          open={updateModalVisible}
-          onCancel={() => setUpdateModalVisible(false)}
-          cancelButtonProps={{style: {display: 'none'}}}
-          okButtonProps={{ style: { display: 'none' } }}
-        >
-           <Form
-            style={{display: 'flex', flexDirection: 'column', alignItems: 'start'}}
-            name="basic"
-            initialValues={{ remember: true }}
-            onFinish={updateApi}
-            autoComplete="off"
-          >
-            <Form.Item
-              label="Route Name"
-              name="routeName"
-              rules={[{ required: true, message: 'Route name is required' }]}
-            >
-              <Input defaultValue={selectedRecord?.routeName}/>
-            </Form.Item>
-            <Form.Item
-              label="Endpoint Path"
-              name="endpointPath"
-              rules={[{ required: true, message: 'Endpoint path is required' }]}
-            >
-              <Input defaultValue={selectedRecord?.endpointPath} />
-            </Form.Item>
-            <Form.Item
-              label="Request Type"
-              name="requestType"
-              rules={[{ required: true, message: 'Request type is required' }]}
-            >
-              <Select
-                defaultValue={selectedRecord?.requestType}
-                style={{ width: 150 }}
-                options={requestType.map((type: string) => {
-                  return {
-                    label: type,
-                    value: type
-                  }
-                })}
-              />
-            </Form.Item>
-            <Form.Item
-              name="dataReturnAllowed"
-              style={{ marginRight: "2rem" }}
-              valuePropName='checked'
-            >
-            <Checkbox style={{color: "black"}}>Return personal and sensible data</Checkbox>
-            </Form.Item>
-            <Button type="primary"  htmlType="submit">Confirm</Button>
-          </Form>
-        </Modal>
+        <ModalUpdateApiComponent
+          updateModalVisible={updateModalVisible}
+          setUpdateModalVisible={setUpdateModalVisible}
+          updateApi={updateApi}
+          selectedRecord={selectedRecord}
+          requestType={requestType}
+        />
       </ApiListContainer>
       <ToastContainer />
     </>
