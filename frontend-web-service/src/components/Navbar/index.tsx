0d5e3f02a07c02f218007688425feac2210568c8
Carlos Souza <carlossouza_94@hotmail.com>
2023-02-16 23:50:35 -0300

    feat: add navbar component

--- frontend-web-service/src/components/Navbar/index.tsx
+++ frontend-web-service/src/components/Navbar/index.tsx
@@ -1,7 +1,50 @@
-import React from 'react'
+import React from 'react';
+import styled from 'styled-components';
 
-export default function Navbar() {
+const NavbarContainer = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  height: 64px;
+  background-color: #333;
+  color: #fff;
+  padding: 0 16px;
+`;
+
+const NavbarTitle = styled.h1`
+  font-size: 24px;
+  margin: 0;
+`;
+
+const NavbarLinks = styled.div`
+  display: flex;
+  align-items: center;
+`;
+
+const NavbarLink = styled.a`
+  color: #fff;
+  text-decoration: none;
+  margin: 0 16px;
+`;
+
+const LogoutButton = styled.button`
+  background-color: transparent;
+  border: none;
+  color: #fff;
+  cursor: pointer;
+`;
+
+function Navbar() {
   return (
-    <div>Navbar</div>
-  )
+    <NavbarContainer>
+      <NavbarTitle>My App</NavbarTitle>
+      <NavbarLinks>
+        <NavbarLink href="/dashboard">Dashboard</NavbarLink>
+        <NavbarLink href="/api">APIs</NavbarLink>
+        <LogoutButton>Logout</LogoutButton>
+      </NavbarLinks>
+    </NavbarContainer>
+  );
 }
+
+export default Navbar;
