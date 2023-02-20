a5367b658fa3d80a76de1f1721a29aafd5db52f6
Carlos Souza <carlossouza_94@hotmail.com>
2023-02-20 02:25:31 -0300

    feat: add configuration controller

---
+++ backend-web-service/src/controllers/configuration/configuration.update.controller.ts
@@ -0,0 +1,23 @@
+import ErrorRes from '../../utils/Erro'
+import { Request, Response } from '../../types/express'
+import ConfigurationEntity from '../../entities/configuration/configuration.entity'
+import { TypeId } from '../../types/mongoose'
+
+class ConfigurationUpdateController {
+  async updateConfiguration (req: Request, res: Response): Promise<Response> {
+    try {
+      const configuration = await ConfigurationEntity.updateConfiguration(req.params._id as unknown as TypeId, req.body)
+
+      if (!configuration) {
+        throw new ErrorRes(400, 'Error updating configuration')
+      }
+
+      return res.status(200).json({ message: 'Configuration updated' })
+    } catch (error) {
+      console.error(error)
+      return res.status(error.status || 500).json({ message: error.message })
+    }
+  }
+}
+
+export default new ConfigurationUpdateController()
---
+++ backend-web-service/src/controllers/configuration/index.ts
@@ -0,0 +1,9 @@
+import ConfigurationCreateController from './configuration.create.controller'
+import ConfigurationUpdateController from './configuration.update.controller'
+import ConfigurationGetController from './configuration.getAll.controller'
+
+export {
+  ConfigurationCreateController,
+  ConfigurationUpdateController,
+  ConfigurationGetController
+}
