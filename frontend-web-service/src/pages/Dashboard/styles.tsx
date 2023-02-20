fdcf3f8e1570c07cf51b781fe357ef915b7d2c17
Carlos Souza <carlossouza_94@hotmail.com>
2023-02-20 02:29:54 -0300

    refactor: add styles and improve the dashboard page

--- frontend-web-service/src/pages/Dashboard/styles.tsx
+++ frontend-web-service/src/pages/Dashboard/styles.tsx
@@ -1,10 +1,12 @@
 import { Card } from 'antd';
 import { HTMLAttributes } from 'react';
+import { LineChart } from 'recharts';
 import styled, { ThemeProps } from 'styled-components';
 
 interface DataApiProps extends HTMLAttributes<HTMLDivElement>, ThemeProps<any> {
   methodColor?: 'GET' | 'POST' | 'DELETE' | 'PUT' | 'PATCH';
 }
+
 
 const ApiContainer = styled.div`
   display: flex;
@@ -13,7 +15,6 @@ const ApiContainer = styled.div`
   background: #27293D;
   color: white;
   border-radius: 10px;
-  background: #27293D;
   margin-top: 2rem;
   margin-bottom: 2rem;
   padding: 1rem;
@@ -30,7 +31,6 @@ const CardItem = styled(Card)`
   background: rgba(0, 0, 0, 0.3);
   border: none;
   color: white;
-  font-size: 18px;
   margin-right: 1rem;
   width: 300;
   div {
@@ -44,7 +44,6 @@ const CardValue = styled.div`
   display: flex;
   flex-direction: row !important;
   align-items: center;
-  /* background: red; */
 `
 
 
@@ -83,11 +82,87 @@ const ApiSearchContainer = styled.div`
     }      
 `
 
+const ErrorContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  height: 100%;
+  background: #27293D;
+  color: white;
+  border-radius: 10px;
+  margin-top: 2rem;
+  margin-bottom: 2rem;
+  padding: 1rem;
+`
+
+const ErrorData = styled.div`
+  display: flex;
+  justify-content: space-between;
+`
+
+const ErrorCard = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  min-height: 50px;
+  min-width: 150px;
+  background: rgba(0, 0, 0, 0.3);
+  border-radius: 4px;
+  margin-left: 1rem;
+  div {
+    color: #ffffff;
+    font-size: 20px;
+    font-weight: bold;
+    margin: 0.5em 0 1em 0;
+  }
+  h4 {
+    color: #7CD4C0;
+    margin: 0 0 0.5rem 0;
+  }
+`
+
+const ErrorLogCard = styled.div`
+  display: flex;
+  font-family: sans-serif;
+  border-radius: 10px;
+  background: white;
+  max-height: 150px;
+  overflow: auto;
+  color: black;
+  margin: 1rem 0 0 0;
+  flex-direction: column;
+  div {
+    background: #ffffff;
+    border: 1px outset #000000;
+    padding: 0.5rem;
+  }
+
+`
+
+const GraphContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  margin-top: 2rem;
+`
+
+const CommonErrorContainer = styled.div`
+  background: rgba(0, 0, 0, 0.3);
+  padding: 1rem;
+  border-radius: 10px;
+  margin: 0 0 1.5rem 0;
+`
+
 export {
   ApiContainer,
   ApiSearchContainer,
   CardItem,
   LabelApi,
   DataApi,
-  CardValue
+  CardValue,
+  ErrorContainer,
+  ErrorCard,
+  ErrorData,
+  GraphContainer,
+  ErrorLogCard,
+  CommonErrorContainer
 }
