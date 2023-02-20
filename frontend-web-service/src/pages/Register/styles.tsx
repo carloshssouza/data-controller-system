7d3d869ea71da0a526330f264de10576dc029951
Carlos Souza <carlossouza_94@hotmail.com>
2023-02-20 02:30:03 -0300

    feat: add new page

---
+++ frontend-web-service/src/pages/Register/styles.tsx
@@ -0,0 +1,13 @@
+import styled from 'styled-components'
+
+export const LoginContainer = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 100vh;
+  text-align: center;
+ 
+  Button {
+    font-weight: bold;
+  }
+`
