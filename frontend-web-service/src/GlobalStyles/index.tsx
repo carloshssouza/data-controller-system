298457374875563506e82c461981099c5f5aa0d2
Carlos Souza <carlossouza_94@hotmail.com>
2023-02-20 23:48:30 -0300

    feat: add transition and new routes

--- frontend-web-service/src/GlobalStyles/index.tsx
+++ frontend-web-service/src/GlobalStyles/index.tsx
@@ -1,6 +1,22 @@
-import { createGlobalStyle } from 'styled-components';
+import styled, { createGlobalStyle, keyframes } from 'styled-components';
 
-export const GlobalStyles = createGlobalStyle`
+const PageTransition = keyframes`
+  from {
+    opacity: 0;
+  }
+  to {
+    opacity: 1;
+  }
+`;
+
+const Container = styled.div`
+  position: relative;
+  min-height: 100vh;
+  opacity: 0;
+  animation: ${PageTransition} 1s ease forwards;
+`;
+
+const GlobalStyles = createGlobalStyle`
   @import url('https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,300;0,700;1,400;1,700&display=swap');
 
   body {
@@ -9,3 +25,8 @@ export const GlobalStyles = createGlobalStyle`
     font-weight: 300;
   }
 `;
+
+export {
+  GlobalStyles,
+  Container
+}
--- frontend-web-service/src/routes/index.tsx
+++ frontend-web-service/src/routes/index.tsx
@@ -1,11 +1,15 @@
 import {BrowserRouter as Router, Routes, Route} from 'react-router-dom'
 import { Dashboard, Api, Login, Error } from '../pages'
+import FirstRegister from '../pages/FirstRegister'
+import RegisterApp from '../pages/RegisterApp'
 
 export default function routes() {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<Login/>}/>
+        <Route path="/" element={<FirstRegister/>}/>
+        <Route path="/register-host" element={<RegisterApp/>}/>
+        <Route path="/login" element={<Login/>}/>
         <Route path="/api" element={<Api/>}/>
         <Route path="/dashboard" element={<Dashboard/>}/>
         <Route path="*" element={<Error/>} />
