6334b11322cfe0831ddf39161012dcadc687b9a3
Carlos Souza <carlossouza_94@hotmail.com>
2023-02-22 00:17:30 -0300

    fix: fix the proxy service

--- backend-web-service/src/utils/Services/ProxyService.ts
+++ backend-web-service/src/utils/Services/ProxyService.ts
@@ -1,5 +1,20 @@
 import { spawn } from 'child_process'
+import path from 'path'
 
-const startProxy = () => spawn('yarn', ['start'], { cwd: '../../../../proxy-server', detached: true })
+const startProxy = () => {
+  const proxyProcess = spawn('gnome-terminal', ['-e', 'yarn start:proxy', '--working-directory', path.resolve(__dirname, '../../../../proxy-server')])
+
+  proxyProcess.stdout.on('data', (data) => {
+    console.log(`stdout: ${data}`)
+  })
+
+  proxyProcess.stderr.on('data', (data) => {
+    console.error(`stderr: ${data}`)
+  })
+
+  proxyProcess.on('close', (code) => {
+    console.log(`child process exited with code ${code}`)
+  })
+}
 
 export default startProxy
