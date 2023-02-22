bcae5886e91e5b5ec6755bc80ce2e2cc9aa13bae
Carlos Souza <carlossouza_94@hotmail.com>
2023-02-22 00:19:18 -0300

    fix: fix the proxy server

---
+++ proxy-server/src/utils/file.ts
@@ -0,0 +1,12 @@
+import fs from 'fs'
+import path from 'path'
+
+class File {
+  public readFile (pathFile: string) {
+    const fileContent = fs.readFileSync(path.resolve(__dirname, pathFile), 'utf8')
+    const config = JSON.parse(fileContent)
+    return config.host
+  }
+}
+
+export default new File()
