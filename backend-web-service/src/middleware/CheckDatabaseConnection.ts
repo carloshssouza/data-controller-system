c7c5875072bf0761d286509a971e9e6580777c2e
Carlos Souza <carlossouza_94@hotmail.com>
2023-02-22 00:15:16 -0300

    feat: add new middleware

---
+++ backend-web-service/src/middleware/CheckDatabaseConnection.ts
@@ -0,0 +1,13 @@
+import { Request, Response, NextFunction } from '../types/express'
+import { mongoose } from '../types/mongoose'
+
+class DatabaseConnection {
+  public checkDbConnection = (req: Request, res: Response, next: NextFunction) => {
+    if (mongoose.connection.readyState !== 1) {
+      return res.status(500).json({ error: 'Database connection not established' })
+    }
+    next()
+  }
+}
+
+export default new DatabaseConnection()
