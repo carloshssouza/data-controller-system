cffca70a25da32e6d85819997915bae87014c135
Carlos Souza <carlossouza_94@hotmail.com>
2023-02-22 00:16:18 -0300

    feat: add new routes

--- backend-web-service/src/routes/login.routes.ts
+++ backend-web-service/src/routes/login.routes.ts
@@ -1,8 +1,11 @@
+import ValidateTokenController from '../controllers/login/validate.token.controller'
 import LoginController from '../controllers/login/login.controller'
 import { Router } from '../types/express'
+import Authenticate from '../middleware/Authenticate'
 
 const routes = Router()
 
 routes.post('/login', LoginController.loginUser)
+routes.get('/validate-token', Authenticate.authenticateCommon, ValidateTokenController.validateToken)
 
 export default routes
