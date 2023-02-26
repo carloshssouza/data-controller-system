10fa2987ed85069feb27a894aa000df35bc3d8d0
Carlos Souza <carlossouza_94@hotmail.com>
2023-02-26 01:30:55 -0300

    refactor: add connection return in middleware

--- backend-web-service/src/middleware/CheckDatabaseConnection.ts
+++ backend-web-service/src/middleware/CheckDatabaseConnection.ts
@@ -4,7 +4,7 @@ import { mongoose } from '../types/mongoose'
 class DatabaseConnection {
   public checkDbConnection = (req: Request, res: Response, next: NextFunction) => {
     if (mongoose.connection.readyState !== 1) {
-      return res.status(500).json({ error: 'Database connection not established' })
+      return res.status(500).json({ error: 'Database connection not established', connection: false })
     }
     next()
   }
