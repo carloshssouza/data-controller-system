53ec533e6050ccb1c282e38e3cd25a4030bdee9a
Carlos Souza <carlossouza_94@hotmail.com>
2023-03-05 16:15:45 -0300

    feat: add navbar

--- frontend-web-service/src/App.tsx
+++ frontend-web-service/src/App.tsx
@@ -1,11 +1,15 @@
 import Routes from './routes'
 import { GlobalStyles } from './GlobalStyles';
+import ConfigurationContext from './context/Configuration/ConfigurationContext';
+import { useState } from 'react';
+import { IConfigurationResponse } from './context/Configuration/interfaces';
+import Navbar from './components/Navbar';
 
 function App() {
   return (
     <>
       <GlobalStyles />
-      <Routes />      
+      <Routes />  
     </>
   )
 }
