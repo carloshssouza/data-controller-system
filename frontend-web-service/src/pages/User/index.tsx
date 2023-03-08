2bf9467c3d718b55c7c2c4b104ba7ce78e2fde2b
Carlos Souza <carlossouza_94@hotmail.com>
2023-03-07 23:52:09 -0300

    feat: add user component

--- frontend-web-service/src/pages/User/index.tsx
+++ frontend-web-service/src/pages/User/index.tsx
@@ -1,11 +1,56 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import api from '../../api/axios'
+import { toast, ToastContainer } from 'react-toastify'
+import { Container } from '../../GlobalStyles'
+import { UserContainer } from './styles'
+import { Button } from 'antd'
+
+interface IUserData {
+  name?: string
+  email?: string
+}
+
 
 export default function User() {
+  const [userData, setUserData] = useState<IUserData>({})
+  const notifyError = (message: string) => toast.error(message);
+  
+  const getUserData = async() => {
+    try {
+      const config = {
+        headers: {
+          Authorization: `Bearer ${localStorage.getItem('token')}`
+        }
+      }
+      const response = await api.get(`${import.meta.env.VITE_BASE_URL}/user/me`, config)
+      if(response.status !== 200) {
+        throw new Error(response.data.message)
+      } else {
+        setUserData(response.data)
+      }
+    } catch (error: any) {
+      notifyError(error.message)
+    }
+  }
+
+  useEffect(() => {
+    getUserData()
+  }, [])
+  
+
   return (
-    <div>
+    <Container>
+      <UserContainer>
       <h1>My account</h1>
-      <div>Name</div>
-      <div>Email</div>
-    </div>
+      <div>
+      <div>{userData.name}</div>
+      <div>{userData.email}</div>
+      </div>
+      <Button>
+        Editar
+      </Button>
+      </UserContainer>
+      <ToastContainer />
+    </Container>
   )
 }
