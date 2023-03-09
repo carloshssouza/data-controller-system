814ef0f7861bf9336f390ddf6a6d62004ab0872c
Carlos Souza <carlossouza_94@hotmail.com>
2023-03-08 23:53:55 -0300

    fix: fix the configuration routes

--- backend-web-service/src/routes/configuration.routes.ts
+++ backend-web-service/src/routes/configuration.routes.ts
@@ -3,8 +3,9 @@ import {
   ConfigurationUpdateController,
   ConfigurationGetController,
   ConfigurationGetDbConnectionController,
-  ConfigurationGetAppHostConnectionController,
-  ConfigurationStartProxyController
+  ConfigurationStartProxyController,
+  ConfigurationAddApplicationHostController,
+  ConfigurationStopProxyController
 } from '../controllers/configuration'
 import { Router } from '../types/express'
 import Authenticate from '../middleware/Authenticate'
@@ -13,10 +14,11 @@ import CheckDatabaseConnection from '../middleware/CheckDatabaseConnection'
 const routes = Router()
 
 routes.get('/configuration', ConfigurationGetController.getConfiguration)
+routes.put('/configuration', Authenticate.authenticateAdmin, ConfigurationUpdateController.updateConfiguration)
+routes.put('/configuration/application-host', Authenticate.authenticateAdmin, ConfigurationAddApplicationHostController.addApplicationHost)
 routes.post('/configuration/mongo-host', ConfigurationCreateController.createConfiguration)
-routes.put('/configuration/app-host', Authenticate.authenticateAdmin, ConfigurationUpdateController.updateConfiguration)
 routes.get('/configuration/db-connection', CheckDatabaseConnection.checkDbConnection, ConfigurationGetDbConnectionController.getDbConnection)
-routes.get('/configuration/app-host-connection', ConfigurationGetAppHostConnectionController.getConfiguration)
 routes.get('/configuration/start-proxy', Authenticate.authenticateAdmin, ConfigurationStartProxyController.startProxy)
+routes.get('configuration/stop-proxy', Authenticate.authenticateAdmin, ConfigurationStopProxyController.stopProxy)
 
 export default routes
