5e127c836643dbcf368c872c9366664cc177c96c
Carlos Souza <carlossouza_94@hotmail.com>
2023-03-14 23:54:38 -0300

    fix: fix the create file

--- backend-web-service/src/utils/Services/FileService.ts
+++ backend-web-service/src/utils/Services/FileService.ts
@@ -4,7 +4,19 @@ import path from 'path'
 class FileService {
   public async createConfigFile (data: any, pathFile: string): Promise<boolean> {
     try {
-      const configString = JSON.stringify({ mongoUrlHost: data })
+      const configString = JSON.stringify({ mongoUriHost: data })
+      await fs.writeFile(path.resolve(__dirname, pathFile), configString)
+      console.log('Config file saved successfully')
+      return true
+    } catch (error) {
+      console.error(`Error writing config file: ${error}`)
+      return false
+    }
+  }
+
+  public async createProxyConfigFile (data: any, pathFile: string): Promise<boolean> {
+    try {
+      const configString = JSON.stringify(data)
       await fs.writeFile(path.resolve(__dirname, pathFile), configString)
       console.log('Config file saved successfully')
       return true
@@ -21,7 +33,7 @@ class FileService {
       throw new Error('Error reading file')
     }
     const config = JSON.parse(fileContent)
-    return config.mongoUrlHost
+    return config.mongoUriHost
   }
 }
 
