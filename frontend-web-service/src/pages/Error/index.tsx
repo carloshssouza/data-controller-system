973c2ef1928eba4599b86b034671751977a9c69f
Carlos Souza <carlossouza_94@hotmail.com>
2023-04-11 01:07:28 -0300

    feat: add new error not found page

--- frontend-web-service/src/pages/Error/index.tsx
+++ frontend-web-service/src/pages/Error/index.tsx
@@ -1,9 +1,19 @@
-import React, { useState } from 'react'
+import { Result, Button } from 'antd';
 
-export default function ErrorLogData() {
+const Error: React.FC = () => {
   return (
-    <div>
-      Not found
-    </div>
-  )
-}
+    <Result
+      status="404"
+      title="404"
+      subTitle="Sorry, the page you visited does not exist."
+      extra={
+        <Button type="primary" onClick={() => window.history.back()}>
+          Go Back
+        </Button>
+      }
+      style={{ color: '#fff' }} // add custom style for text color
+    />
+  );
+};
+
+export default Error;
