4b955dddc8f95bd65ba1381e7d3e700b0c1cc76c
Carlos Souza <carlossouza_94@hotmail.com>
2023-05-04 23:20:01 -0300

    feat: add new method in configuration repository

--- backend-web-service/src/repositories/configuration.repository.ts
+++ backend-web-service/src/repositories/configuration.repository.ts
@@ -21,6 +21,15 @@ class ConfigurationRepository {
     }
   }
 
+  public async updateConfigurationMongoUri (mongoUri: string) {
+    const configuration = await Configuration.find({}) as any
+    if (!configuration) {
+      throw new Error('Configuration not found')
+    } else {
+      return Configuration.findOneAndUpdate({ _id: configuration[0]._id }, { mongoUriHost: mongoUri })
+    }
+  }
+
   public async deleteApplicationHost () {
     const configuration = await Configuration.find({}) as any
     if (!configuration) {
@@ -35,7 +44,7 @@ class ConfigurationRepository {
     if (!configuration) {
       throw new Error('Configuration not found')
     } else {
-      const deleteConfiguration = Configuration.findOneAndUpdate({ _id: configuration[0]._id }, { mongoUriHost: '' })
+      const deleteConfiguration = await Configuration.findOneAndUpdate({ _id: configuration[0]._id }, { mongoUriHost: '' })
       if (deleteConfiguration) {
         Database.disconnect()
         return true
