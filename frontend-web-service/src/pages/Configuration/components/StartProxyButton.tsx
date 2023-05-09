5bcbea16fad8367fd56f611959ba8e270f3a2214
Carlos Souza <carlossouza_94@hotmail.com>
2023-05-08 23:56:57 -0300

    feat: add new button to stop and start proxy

---
+++ frontend-web-service/src/pages/Configuration/components/StartProxyButton.tsx
@@ -0,0 +1,25 @@
+import { LoadingOutlined } from '@ant-design/icons';
+import { StyledButton } from './styles';
+import { Spin } from 'antd';
+
+interface StartProxyButtonProps {
+  isProxyStarted: boolean;
+  isLoading: boolean;
+  isApplicationHostStarted: boolean;
+  startProxy: () => Promise<void>;
+}
+
+const StartProxyButton = ({isProxyStarted, isLoading, isApplicationHostStarted, startProxy}: StartProxyButtonProps) => {
+  return (
+    <StyledButton 
+      type="primary"
+      typeButton="start" 
+      disabled={isProxyStarted || isLoading || !isApplicationHostStarted}
+      onClick={startProxy}
+    >
+      { isLoading ? <LoadingOutlined /> : 'Start proxy'}
+    </StyledButton>
+  );
+};
+
+export default StartProxyButton;
--- frontend-web-service/src/pages/Configuration/components/styles.tsx
+++ frontend-web-service/src/pages/Configuration/components/styles.tsx
@@ -2,27 +2,41 @@ import styled from "styled-components"
 import { Button } from "antd"
 
 interface IStyledButton {
-  isProxyStarted: boolean
+  typeButton: string
   disabled: boolean
 }
 
-const StyledButton = styled(Button) <IStyledButton>`
+const StyledButton = styled(Button)<IStyledButton>`
   background: ${(props) => {
-    if (props.isProxyStarted && !props.disabled) {
-      return '#b6b83b !important'
-    } else if(props.isProxyStarted && !props.disabled) {
-      return '#4f5023 !important'
-    } else if (!props.isProxyStarted && !props.disabled) {
-      return '#874ccb !important'
-    } else if (!props.isProxyStarted && props.disabled) {
-      return '#543d6e !important'
+      if (props.typeButton === 'start' && !props.disabled) {
+        return '#874ccb !important'
+      } else if (props.typeButton === 'stop' && !props.disabled) {
+        return '#b89f3b !important'
+      } else if (props.typeButton === 'start' && props.disabled) {
+        return '#482b6a !important'
+      } else if (props.typeButton === 'stop' && props.disabled) {
+        return '#4f5023 !important'
+      }
     }
-  }
   };
   border: none;
   font-weight: bold;
   color: white;
-  margin: 1rem 0 1rem 0 !important;
+  margin: 1rem 0.5rem 1rem 0 !important;
+  :hover {
+    background: ${(props) => {
+      if (props.typeButton === 'start' && !props.disabled) {
+        return '#a682d0 !important'
+      } else if (props.typeButton === 'stop' && !props.disabled) {
+        return '#d5be60 !important'
+      } else if (props.typeButton === 'start' && props.disabled) {
+        return '#482b6a !important'
+      } else if (props.typeButton === 'stop' && props.disabled) {
+        return '#4f5023 !important'
+      }
+    }
+  };
+  }
 `
 
 export {
