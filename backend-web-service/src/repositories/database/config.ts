3302f5724577976c95f031f0a50ed5f0aa8148e7
Carlos Souza <carlossouza_94@hotmail.com>
2023-05-21 13:35:56 -0300

    fix: fix the database

--- backend-web-service/src/repositories/database/config.ts
+++ backend-web-service/src/repositories/database/config.ts
@@ -1,12 +1,27 @@
+import configurationEntity from '../../entities/configuration/configuration.entity'
+import UserEntity from '../../entities/user/user.entity'
 import { mongoose } from '../../types/mongoose'
+import defaultUser from '../utils/defaultUser'
+import restrictDataList from '../utils/defaultRestrictDataList'
 
 class Database {
   public async connect (url: string): Promise<boolean> {
     mongoose.set('strictQuery', true)
 
     return mongoose.connect(url)
-      .then(() => {
+      .then(async () => {
         console.log('Connected to Mongodb')
+        const user = await UserEntity.getDefaultUser('admin')
+        if (!user) {
+          const createUser = UserEntity.createUser(defaultUser)
+          const createRestrictData = configurationEntity.createConfiguration(restrictDataList)
+          if (createRestrictData) {
+            console.log('Default restrict data created')
+          }
+          if (createUser) {
+            console.log('Default user created')
+          }
+        }
         return true
       })
       .catch((err) => {
