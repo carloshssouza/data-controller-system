2b4bb44651968130a63c5f67305ab73d88a7f889
Carlos Souza <carlossouza_94@hotmail.com>
2023-05-21 13:37:46 -0300

    docs: doc frontend

--- frontend-web-service/vite.config.ts
+++ frontend-web-service/vite.config.ts
@@ -1,7 +1,14 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-
+import dotenv from 'dotenv'
+dotenv.config()
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    port: process.env.VITE_DEV_SERVER_PORT as unknown as number || 3000
+  },
+  preview: {
+    port: process.env.VITE_DEV_SERVER_PORT as unknown as number || 3000
+  }
 })
